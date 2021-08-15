ddc80d6bdc3be5af58c2587e21a5b9e2ef54b0ca
Peter Isaiah <peterisaiah4fun@gmail.com>
2021-08-15 23:18:17 +0100

    sidebar

--- src/component/sidebar/Sidebar.jsx
+++ src/component/sidebar/Sidebar.jsx
@@ -1,9 +1,52 @@
 import './sidebar.css'
+import { Chat } from '@material-ui/icons'
 
 function Sidebar() {
-    return (
-        <div className='sidebar' >sidebar</div>
-    )
+  return (
+    <div className='sidebar'>
+      <div className="wrapper">
+        <ul className="list">
+          <li className="items">
+            <Chat className='icon' />
+            <span className="text">Chat</span>
+          </li>
+        </ul>
+        <hr className="sidebarhr" />
+        <ul className="friendList">
+          <li className="friend">
+            <img src="/asset/person/2.jpg" alt="" className="profilepicture" />
+            <span className="username">jane</span>
+          </li>
+          <li className="friend">
+            <img src="/asset/person/3.jpg" alt="" className="profilepicture" />
+            <span className="username">jane</span>
+          </li>
+          <li className="friend">
+            <img src="/asset/person/4.jpg" alt="" className="profilepicture" />
+            <span className="username">jane</span>
+          </li>
+          <li className="friend">
+            <img src="/asset/person/5.jpg" alt="" className="profilepicture" />
+            <span className="username">jane</span>
+          </li>
+          <li className="friend">
+            <img src="/asset/person/6.jpg" alt="" className="profilepicture" />
+            <span className="username">jane</span>
+          </li>
+          <li className="friend">
+            <img src="/asset/person/2.jpg" alt="" className="profilepicture" />
+            <span className="username">jane</span>
+          </li>
+          <li className="friend">
+            <img src="/asset/person/2.jpg" alt="" className="profilepicture" />
+            <span className="username">jane</span>
+          </li>
+          
+        </ul>
+      </div>
+
+    </div>
+  )
 }
 
 export default Sidebar
