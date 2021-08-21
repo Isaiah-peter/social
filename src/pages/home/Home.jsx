393f3025926ff911313b428f1319337476a92548
Peter Isaiah <peterisaiah4fun@gmail.com>
2021-08-21 22:21:39 +0100

    working datA

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -13,14 +13,14 @@ function Home() {
  
    const getPost = async () => {
      try {
-       const res = await axios.get("http://Localhost:8000/user", {
+       const res = await axios.get("http://Localhost:8000/timeline", {
          headers: {
            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOjIsIklzQWRtaW4iOmZhbHNlLCJleHAiOjE2MzUzMjI2MzV9.DxTSqbAdHQrbSu7Bws1ClJVeyBZ14Z1WEbdnyPMLvQc"
          }
        }
        )
  
-       console.log(res)
+       console.log(res.data)
  
      }
      catch (err) {
